29e271b68ea63773075c212fb0ca7003ec3c98a4
Leonardo Sanchez <v-lsanchez@youngliving.com>
2021-07-14 18:47:42 -0500

    initial commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,37 @@
+﻿using fone_soap_app.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fone_soap_app.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private ISoapDemo _soapDemo;
+        private INumberConversion _numberConversion;
+        private IDian _dian;
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ISoapDemo soapDemo, INumberConversion numberConversion,IDian dian, ILogger<WeatherForecastController> logger)
+        {
+            _soapDemo = soapDemo;
+            _logger = logger;
+            _numberConversion = numberConversion;
+            _dian = dian;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            var result = await _dian.GetStatus("2546");
+            return new ObjectResult(result);
+        }
+    }
+}
---
+++ Services/Dian.cs
@@ -0,0 +1,53 @@
+﻿using DianServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace fone_soap_app.Services
+{
+
+    public interface IDian
+    {
+        Task<WcfDianCustomerServicesClient> GetInstanceAsync();
+        Task<DianResponse> GetStatus(string trackId);
+    }
+
+
+    public class Dian : IDian
+    {
+
+        public readonly string serviceUrl = "https://vpfe-hab.dian.gov.co/WcfDianCustomerServices.svc?wsdl";
+        public readonly EndpointAddress endpointAddress;
+        public readonly BasicHttpBinding basicHttpBinding;
+
+        public Dian()
+        {
+            endpointAddress = new EndpointAddress(serviceUrl);
+
+            basicHttpBinding =
+                new BasicHttpBinding(endpointAddress.Uri.Scheme.ToLower() == "http" ?
+                            BasicHttpSecurityMode.None : BasicHttpSecurityMode.Transport);
+
+            //Please set the time accordingly, this is only for demo
+            basicHttpBinding.OpenTimeout = TimeSpan.MaxValue;
+            basicHttpBinding.CloseTimeout = TimeSpan.MaxValue;
+            basicHttpBinding.ReceiveTimeout = TimeSpan.MaxValue;
+            basicHttpBinding.SendTimeout = TimeSpan.MaxValue;
+        }
+
+        public async Task<WcfDianCustomerServicesClient> GetInstanceAsync()
+        {
+            return await Task.Run(() => new WcfDianCustomerServicesClient(basicHttpBinding, endpointAddress));
+        }
+
+
+        public async Task<DianResponse> GetStatus(string  trackId)
+        {
+            var client = await GetInstanceAsync();
+            var response = await client.GetStatusAsync(trackId);
+            return response;
+        }
+    }
+}
